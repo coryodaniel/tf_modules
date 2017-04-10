78488fcf72005489931e479c1ee7ea3e5583ac28
Cory O'Daniel <cory@coryodaniel.com>
2017-04-10 12:34:52 -0700

    Moved ebs_enabled into options block

--- firehose_to_es/elasticsearch.tf
+++ firehose_to_es/elasticsearch.tf
@@ -1,10 +1,9 @@
 resource "aws_elasticsearch_domain" "instance" {
   domain_name           = "${var.es_domain_name}"
   elasticsearch_version = "${var.es_version}"
-  ebs_enabled = false
 
-  advanced_options {
-    "rest.action.multi.allow_explicit_index" = true
+  ebs_options{
+    ebs_enabled = false
   }
 
   snapshot_options {
